5c8912646910f0478ec3ba96deb11f5658f4ea7f
notanerd <166221302+notanerd314@users.noreply.github.com>
2025-05-19 16:55:16 +0700

    update

--- ask8ball/components/CustomizeDialog.tsx
+++ ask8ball/components/CustomizeDialog.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { useRef, forwardRef, useImperativeHandle } from 'react';
-import { GlobalContext } from './GlobalContext';
+import { useRef, forwardRef, useImperativeHandle, ChangeEvent } from 'react';
+import { useGlobal } from './GlobalContext';
 import '../styles/globals.css'
 
 const CustomizeDialog = forwardRef<HTMLDialogElement, React.HTMLAttributes<HTMLDialogElement>>((props, ref) => {
+  const { allAnswers, setAllAnswers } = useGlobal();
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   useImperativeHandle(ref, () => dialogRef.current as HTMLDialogElement);
+
+  function changeAllAnswers(element: ChangeEvent<HTMLTextAreaElement>) {
+    const listAnswers = element.target.value.split('\n')
+                        .map(line => line.trim())
+                        .filter(line => line);
+    setAllAnswers(listAnswers);
+  }
 
   return (
     <dialog ref={dialogRef} onClose={() => dialogRef.current?.close()} {...props}>
@@ -15,7 +23,7 @@ const CustomizeDialog = forwardRef<HTMLDialogElement, React.HTMLAttributes<HTMLD
       <p>Name of the custom magic 8 ball:</p>
       <input id="title-textarea" placeholder="Magic 8 Ball"></input>
       <p>All possible responses:</p>
-      <textarea id="options-textarea" placeholder="Click enter for next response"></textarea>
+      <textarea value={allAnswers.join("\n")} id="options-textarea" placeholder="Click enter for next response" onChange={changeAllAnswers}></textarea>
       <button className="dialogClose" onClick={() => dialogRef.current?.close()}>x</button>
     </dialog>
   )
