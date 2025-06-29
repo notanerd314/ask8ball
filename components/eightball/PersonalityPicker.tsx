00c3eae5f34cfb2d6bff52608a70dee785f88b05
notanerd <ngoca0309@outlook.com>
2025-06-29 13:47:31 +0700

    Make all items in personality picker the same height

--- components/eightball/PersonalityPicker.tsx
+++ components/eightball/PersonalityPicker.tsx
@@ -1,21 +1,21 @@
 import { PersonalityConfig } from "../../lib/prompts"
 import { getAllPersonalities } from "../../lib/api"
+
 import Link from "next/link"
 
 export function Personality({ personality }: { personality: PersonalityConfig }) {
   return (
     <Link
       href={`/play/${personality.linkname}`}
-      className={`block p-4 !text-white rounded-xl h-full hover:scale-105 hover:-translate-y-2 transition-transform border-2 border-slate-700 bg-gradient-to-br from-slate-800 to-slate-900 ${personality.theme.hoverBackground} active:translate-0`}
+      className={`block p-4 !text-white rounded-xl h-45 hover:scale-105 hover:-translate-y-2 transition-transform border-2 border-slate-700 bg-gradient-to-br from-slate-800 to-slate-900 ${personality.theme.hoverBackground} active:translate-0 leading-normal text-center`}
       aria-label={`Choose ${personality.name} personality`}
       title={`Choose ${personality.name} personality`}
       role="link"
     >
-      <div className="flex flex-col w-full gap-1 text-center">
-        <span className="w-auto text-5xl">{personality.theme.icon}</span>
-        <h2 className="text-3xl font-bold">{personality.name}</h2>
-        <p>{personality.description}</p>
-      </div>
+      <span className="w-auto text-5xl">{personality.theme.icon}</span>
+      <h2 className="text-3xl font-bold">{personality.name}</h2>
+      <p>{personality.description}</p>
+
     </Link>
   )
 }
@@ -27,7 +27,8 @@ export default async function PersonalityPicker() {
   return (
     <section>
       <h1 className='text-center'>Try more personalities</h1>
-      <div className='grid grid-cols-2 gap-5 p-5 mx-auto xl:w-5xl lg:w-4xl md:grid-cols-3'>
+
+      <div className='grid grid-cols-2 gap-6 p-5 mx-auto xl:w-5xl lg:w-4xl md:grid-cols-3 items-stretch'>
         {personalitiesList.map((personality, index) => (
           <Personality key={index} personality={personality} />
         ))}
