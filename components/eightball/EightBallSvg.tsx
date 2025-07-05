c0c00d8e62c0eaffe6c65e61c6b9bc35b53924aa
notanerd <ngoca0309@outlook.com>
2025-07-05 10:49:23 +0700

    Small tweaks and change Childish's background

--- components/eightball/EightBallSvg.tsx
+++ components/eightball/EightBallSvg.tsx
@@ -74,7 +74,7 @@ export const EightBallSvg: React.FC<Props> = ({ currentState, diceColor, diceSty
       </defs>
       <ellipse cx="568" cy="454" fill="url(#bgGrad)" rx="220" ry="220" />
 
-      <g className="transform scale-125 md:scale-100 origin-center md:translate-0 -translate-y-20 -translate-x-28">
+      <g className="transform scale-125 md:scale-100 origin-center md:translate-0 -translate-y-20 -translate-x-29">
         <ellipse cx="568" cy="454" fill={canShowResult ? "url(#borderGrad)" : "white"} rx="115" ry="113" />
         <path fill={canShowDefault ? "url(#innerBackgroundGrad)" : "url(#innerDiceBackgroundGrad)"} stroke="transparent" strokeMiterlimit="10" d="M678 454.4c1.4-58.6-48.9-107-109.8-105.5-61-1.4-111.2 47-109.7 105.5-1.5 58.5 48.8 106.9 109.7 105.5 61 1.4 111.2-46.7 109.7-105.5z" />
         <path fill={canShowResult ? diceColor : "none"} style={diceStyle} ref={diceRef} stroke="transparent" d="M481.3 402.7c-3.1-5.7.3-10.4 8.2-10.7 50.6-1.6 107.6-1.6 157.4.3 7.6.2 11 5.2 7.9 10.6a2332.4 2332.4 0 0 1-80 131.1c-3.6 5.7-9.6 5.7-13.3 0a2645.3 2645.3 0 0 1-80.1-131.3z" />
--- components/eightball/PersonalityInfo.tsx
+++ components/eightball/PersonalityInfo.tsx
@@ -7,9 +7,10 @@ export default function PersonalityInfo() {
 
   return (
     <div className='text-center leading-relaxed text-[1.25rem]'>
-      <h2>
+      <h2 className={`font-bold ${currentPersonality.tag && "flex items-center justify-center mt-2 mb-1"}`}>
         <span className="mr-3">{currentPersonality.theme.icon}</span>
-        {currentPersonality.name}
+        <span className="text-4xl">{currentPersonality.name}</span>
+        {currentPersonality.tag && <mark className="!text-[0.95rem] ml-3">{currentPersonality.tag}</mark>}
       </h2>
       <p>{currentPersonality.description}</p>
     </div>
