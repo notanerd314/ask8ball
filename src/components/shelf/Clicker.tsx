a116283c200c0d963031196f21901946e5712880
notanerd <ngoca0309@outlook.com>
2025-07-31 14:51:45 +0700

    Add click sound to the Clicker

--- src/components/shelf/Clicker.tsx
+++ src/components/shelf/Clicker.tsx
@@ -1,19 +1,39 @@
 "use client"
 
 import { useState } from "react";
+import useSound from "use-sound";
 
 export default function Clicker() {
   const [count, setCount] = useState(0);
+  const [play] = useSound("/sounds/click.mp3", {
+    volume: 0.5, // optional, adjust as needed
+    interrupt: false, // allow overlaps
+  });
+
+  const clickAndPlay = () => {
+    setCount((prev) => prev + 1);
+    play();
+  };
+
+  const evilClickAndPlay = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.button === 1) {
+      e.preventDefault();
+      setCount((prev) => prev - 1);
+      play();
+    }
+  };
 
   return (
     <button
-      onClick={() => setCount(count + 1)}
+      onClick={clickAndPlay}
+      onAuxClick={evilClickAndPlay}
       className="
         px-4 py-2 text-2xl text-black
-        bg-gray-300 border border-gray-500 rounded-md
+        bg-gradient-to-b from-gray-200 to-gray-300 border border-gray-500 rounded-md
         shadow-[inset_-2px_-2px_0px_#ffffff,2px_2px_0px_#a1a1aa]
         active:shadow-[inset_2px_2px_0px_#a1a1aa,inset_-2px_-2px_0px_#ffffff]
-        transition-all duration-100 ease-in-out
+        active:from-gray-300 active:to-gray-200
+        transition-all duration-100 ease-in-out max-w-55
       "
       title="Click me!"
       aria-label="Click me!"
