47083cffe01e51bb002c62bdb3149b1262faf5fd
notanerd <ngoca0309@outlook.com>
2025-07-31 14:54:06 +0700

    Create unfinished "ADHD Weather Broadcast" project

--- src/app/adhd-weather/layout.tsx
+++ src/app/adhd-weather/layout.tsx
@@ -1,3 +1,4 @@
+import './globals.css'
 import Logo from '@/components/Logo';
 
 export const metadata = {
--- src/app/adhd-weather/page.tsx
+++ src/app/adhd-weather/page.tsx
@@ -2,15 +2,21 @@ import VideoBackground from "@/components/adhd-weather/VideoBackground";
 
 export default function Home() {
   return (
-    <>
-      <VideoBackground videoUrl="/videos/adhd-weather/clear_sky.webm" />
+    <main className="w-screen h-screen flex items-center justify-center relative">
+      <VideoBackground videoUrl="/videos/adhd-weather/defaultbg.webm" />
 
-      <form className="absolute top-1/2 left-1/2 -translate-1/2 bg-black/50 text-white p-8 rounded-2xl text-center backdrop-blur-md">
-        <h1 className="!text-4xl font-bold mb-2">ADHD Weather Broadcast</h1>
-        <h2>Summarizes the weather in 5 words or less.</h2>
+      <form className="bg-black/50 text-white p-8 rounded-2xl text-center backdrop-blur-md max-w-2xl m-10 w-full z-10">
+        <h1 className="font-bold mb-2">ADHD Weather Broadcast</h1>
+        <h2 className="text-2xl">Summarizes the weather in 5 words or less.</h2>
         <br />
-        <input type="text" placeholder="Enter your location" className="p-3 bg-black/40 rounded-2xl w-full text-white placeholder:text-gray-400" />
+        <input
+          type="text"
+          placeholder="Enter your location"
+          className="p-3 bg-black/40 rounded-2xl w-full text-white placeholder:text-gray-400"
+          required aria-required
+          aria-label="Enter your location"
+        />
       </form>
-    </>
-  )
+    </main>
+  );
 }
