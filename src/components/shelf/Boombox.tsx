b49b7de07f729e3755eb1b6a77445d4120e8382c
notanerd <ngoca0309@outlook.com>
2025-07-31 14:51:22 +0700

    Add disabling boombox when fading in/out song

--- src/components/shelf/Boombox.tsx
+++ src/components/shelf/Boombox.tsx
@@ -6,6 +6,7 @@ import { useRef, useState } from "react";
 export default function Boombox() {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isToggling, setIsToggling] = useState(false);
 
   const fadeInAndPlay = () => {
     if (!audioRef.current) {
@@ -24,11 +25,16 @@ export default function Boombox() {
         audioRef.current.volume += step;
       } else {
         audioRef.current.volume = 1;
+        setIsToggling(false);
         clearInterval(interval);
       }
     }, 100);
   };
 
+  /**
+   * Gradually reduce the audio volume to 0, then pause the audio.
+   * @returns {void}
+   */
   const fadeOutAndPause = () => {
     if (!audioRef.current) return;
 
@@ -39,6 +45,7 @@ export default function Boombox() {
       } else {
         audioRef.current!.volume = 0;
         audioRef.current!.pause();
+        setIsToggling(false);
         clearInterval(interval);
       }
     }, 100);
@@ -46,6 +53,7 @@ export default function Boombox() {
 
 
   const toggleAudio = () => {
+    setIsToggling(true);
     if (isPlaying) {
       fadeOutAndPause();
     } else {
@@ -60,6 +68,7 @@ export default function Boombox() {
       onClick={toggleAudio}
       className="ml-5 cursor-pointer duration-100 hover:-translate-y-2 transition-transform max-w-40"
       title={isPlaying ? "Pause it" : "Hit the beat"}
+      disabled={isToggling}
     >
       <img
         src="/images/boombox.webp"
