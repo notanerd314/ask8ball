cb6d51e4c58ab121d70eb29ac840413537d529bb
notanerd314 <ngoca0309@outlook.com>
2025-10-02 23:12:06 +0700

    Do phaser later

--- apps/stupid-sandbox/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-import tailwind from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
-const config = {
-  plugins: [tailwind, autoprefixer],
-}
-
-export default config
--- apps/stupid-sandbox/src/app.tsx
+++
@@ -1,8 +0,0 @@
-export function App() {
-
-  return (
-    <>
-      <div id="game-container" className="absolute inset-0"></div>
-    </>
-  )
-}
